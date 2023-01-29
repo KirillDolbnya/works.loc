fe4ae88d3be70b5eb66d8ea77c93fae53376b91b
KirillDolbnya <kirilldolbnya6@gmail.com>
2023-01-29 20:31:00 +0300

    ООП php

--- ООП/index.php
+++ ООП/index.php
@@ -4,9 +4,16 @@ class person {
     public $name;
     public $age;
 
+    const ID = 10;
+
+
+    public function __construct($name,$age){
+        $this->name = $name;
+        $this->age = $age.'<br>';
+    }
 
     public function sayHello(){
-        $result = 'Привет меня зовут '.$this->name.' мне '.$this->age.' лет';
+        $result = 'Привет меня зовут '.$this->name.' мне '.$this->age.' лет'.' '.$this->sayAge();
         return $result;
     }
 
@@ -16,20 +23,58 @@ class person {
 
     public function setAge($age){
         $this->age = $age;
+    }   
+
+    public function sayAge(){
+        return $this->age;
+    }
+
+    public static function staticFunction(){
+        echo 'i am static function'.'<br>';
     }
 
 
 }
 
-$user = new person;
+$user = new person('Kirill' , 18);
 
-$user->setName('Kirill');
+$user2 = new person('Tagir', 19);
 
-$user->name;
+echo $user->name;
+echo $user->age;
 
-$user->setAge(18);
+echo $user2->name;
+echo $user2->age;
 
-$user->age;
+echo $user::ID.'<br>';
 
-echo $user->sayHello();
+person::staticFunction();
+
+//$user->setName('Kirill');
+//
+//$user2->setName('Tagit');
+//
+//$user->setAge(18);
+//
+//$user2->setAge(19);
+//
+//echo $user->sayHello().'<br>';
+//
+//echo $user2->sayHello();
+
+
+class university {
+
+    public function student(person $students){
+        echo $students->name;
+    }
+}
+
+
+$students = new person('Kirill' , 18);
+
+$univer = new university();
+
+$univer->student($students);
+
 ?>
