bdf3a90b4acf46267176e9ad501ebf52dc0a79a8
DESKTOP-O09VUGK\Студент <kirilldolbnya6@gmail.com>
2023-03-03 11:59:31 +0300

    functional register

---
+++ exercise/cleanOOP/includes/init.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once('../class/config.php');
+include_once('../class/database.php');
+include_once('../class/input.php');
+include_once('../class/validate.php');
+include_once('../class/session.php');
+include_once('../class/token.php');
+include_once ('../class/user.php');
+include_once ('../class/redirect.php');
---
+++ exercise/cleanOOP/includes/register.php
@@ -0,0 +1,119 @@
+<?php
+session_start();
+
+require_once ('init.php');
+
+
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'marlinOOP',
+        'something' => [
+            'no' => [
+                'foo' => [
+                    'bar' => 'baz',
+                ],
+            ],
+        ],
+    ],
+    'session' => [
+        'token_name' => 'token',
+    ]
+];
+
+if (Input::exists()) {
+    if (Token::check(Input::get('token'))) {
+        $validate = new Validate();
+
+        $validation = $validate->check($_POST, [
+            'username' => [
+                'required' => true,
+                'min' => 2,
+                'max' => 15,
+                'unique' => 'users',
+            ],
+            'email' => [
+                'required' => true,
+                'email' => true,
+                'unique' => 'users',
+            ],
+            'password' => [
+                'required' => true,
+                'min' => 3,
+            ],
+            'repeat_password' => [
+                'required' => true,
+                'mathes' => 'password',
+            ]
+        ]);
+
+        if ($validation->passed()) {
+            $user = new User();
+
+            $user->create([
+                    'username' => Input::get('username'),
+                    'password' => password_hash(Input::get('password') , PASSWORD_DEFAULT),
+                    'email' => Input::get('email')
+            ]);
+            Session::flash('success', 'register success');
+//            Redirect::to('test.php');
+//            Redirect::to(404);
+        } else {
+            foreach ($validation->errors() as $error) {
+                echo $error . '<br>';
+            }
+        }
+    }
+}
+
+//$users = Database::Instance()->query("SELECT * FROM users WHERE username IN (?,?)",['Tagir','Kirill']);
+//$users = Database::Instance()->get('users',['username','=','Tagir']);
+//var_dump($users->count());die();
+
+//$id = 3;
+//$users = Database::Instance()->update('users',$id,[
+//    'username' => 'Marlin1',
+//    'password'=> '123',
+//]);
+
+//if ($users->error()) {
+//    echo 'Ошибка';
+//}else{
+//    foreach ($users->result() as $user) {
+//        echo $user['id'] . " " . $user['username'] . "<br>";
+//    }
+//}
+
+//echo $users->result()[0]['username'];
+//echo $users->first();
+
+?>
+
+
+<form action="" method="post">
+
+    <?php //echo Session::flash('success'); ?>
+
+    <div>
+        <label for="username">username</label><br>
+        <input type="text" name="username" value="<?php echo Input::get('username') ?>">
+    </div>
+    <div>
+        <label for="email">email</label><br>
+        <input type="text" name="email">
+    </div>
+    <div>
+        <label for="password">password</label><br>
+        <input type="text" name="password">
+    </div>
+    <div>
+        <label for="repeat_password">repeat password</label><br>
+        <input type="text" name="repeat_password">
+    </div>
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+    <div style="margin-top: 10px">
+        <button type="submit">register</button>
+    </div>
+</form>
