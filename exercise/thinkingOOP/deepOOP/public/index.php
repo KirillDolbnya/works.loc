d61d947047aee27e498e3ce8e2d80b179c70a240
DESKTOP-O09VUGK\admin <kirilldolbnya6@gmail.com>
2023-03-15 16:47:17 +0300

    update querybilder index composer

--- exercise/thinkingOOP/deepOOP/public/index.php
+++ exercise/thinkingOOP/deepOOP/public/index.php
@@ -1,36 +1,46 @@
 <?php
 
-
 //include_once '../functions.php';
-////dd($posts);
+//dd($posts);
+
+//var_dump($_SERVER);
+
+//include '../classes/bar/Order.php';
+//include '../classes/too/Order.php';
 //
-////var_dump($_SERVER);
+//use Bar\OrderClass\Order as Bar;
+//use Too\OrderClass\Order as Too;
+//
+//$class_one = new Bar();
+//$class_two = new Too();
+//
+//echo '<hr>';
+//echo '<hr>';
+//echo '<hr>';
+//
+//spl_autoload_register(function ($class) {
+//    var_dump($class);
+////    die();
+//    include '../classes/' . $class . '.php';
+//});
+//
+//$class1 = new ClassOne();
+//$class2 = new ClassTwo();
+//
+//echo '<hr>';
+//echo '<hr>';
+//echo '<hr>';
 
-include '../classes/bar/Order.php';
-include '../classes/too/Order.php';
 
-use Bar\OrderClass\Order as Bar;
-use Too\OrderClass\Order as Too;
+require_once "../vendor/autoload.php";
+use App\QueryBuilder;
+$db = new QueryBuilder();
 
-$class_one = new Bar();
-$class_two = new Too();
 
-echo '<hr>';
-echo '<hr>';
-echo '<hr>';
+$result = $db->getAll('posts');
 
-spl_autoload_register(function ($class) {
-    var_dump($class);
-//    die();
-    include '../classes/' . $class . '.php';
-});
+var_dump($result);
 
-$class1 = new ClassOne();
-$class2 = new ClassTwo();
-
-echo '<hr>';
-echo '<hr>';
-echo '<hr>';
 
 //$routes = [
 //    '/exercise/thinkingOOP/home'=>'function/homepage.php',
