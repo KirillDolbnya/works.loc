93e178e20f47fdfb2060d27f2389dc0c39ca80cd
DESKTOP-O09VUGK\admin <kirilldolbnya6@gmail.com>
2023-03-20 11:35:54 +0300

    add directory

--- exercise/deepOOP/public/index.php
+++ exercise/deepOOP/public/index.php
@@ -1,15 +1,20 @@
 <?php
 require_once "../vendor/autoload.php";
-use App\QueryBuilder;
-$db = new QueryBuilder();
+//use App\QueryBuilder;
+//$db = new QueryBuilder();
+//
+//
+//$result = $db->getAll('posts');
+//
+////var_dump($result);
+//$db->delete(9,'posts');
+echo 'index';
+//echo '<pre>';
+//var_dump($_SERVER);
+//echo '</pre>';
 
 
-$result = $db->getAll('posts');
-
-//var_dump($result);
-$db->delete(9,'posts');
-
-//if ($_SERVER['REQUEST_URI'] == 'home'){
-//    require '../app/controllers/homepage.php';
-//}
-//exit;
+if ($_SERVER['REQUEST_URI'] == '/exercise/deepOOP/public/home'){
+    require '../App/controllers/homepage.php';
+}
+exit;
