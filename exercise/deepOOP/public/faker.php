7ecb819c40fc8f7e2c31501e39b6c074313120b2
DESKTOP-O09VUGK\admin <kirilldolbnya6@gmail.com>
2023-04-05 15:44:12 +0300

    add pagination

--- exercise/deepOOP/public/faker.php
+++ exercise/deepOOP/public/faker.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
 <?php
@@ -38,6 +39,14 @@ $queryFactory = new QueryFactory('mysql');
 $select = $queryFactory->newSelect();
 $select
     ->cols(['*'])
+    ->from('posts');
+$sth = $pdo->prepare($select->getStatement());
+$sth->execute($select->getBindValues());
+$totalItems = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+$select = $queryFactory->newSelect();
+$select
+    ->cols(['*'])
     ->from('posts')
     ->setPaging(3)
     ->page($_GET['page'] ?? 1);
@@ -45,13 +54,51 @@ $select
 $sth = $pdo->prepare($select->getStatement());
 $sth->execute($select->getBindValues());
 $items = $sth->fetchAll(PDO::FETCH_ASSOC);
-var_dump($items);
 
-foreach ($items as $item => $value){
-    echo '<p>'.$value['title'].' это title</p>';
-    echo '<p>'.$value['content'].' это content</p>';
+
+//foreach ($items as $item => $value){
+//    echo '<p>'.$value['title'].' это title</p>';
+//    echo '<p>'.$value['content'].' это content</p>';
+//}
+
+$itemsPerPage = 3;
+$currentPage = $_GET['page'] ?? 1;
+$urlPattern = '?page=(:num)';
+
+$paginator = new Paginator(count($totalItems), $itemsPerPage, $currentPage, $urlPattern);
+foreach ($items as $item) {
+    echo $item['id'] . PHP_EOL . $item['title'] . '<br>';
 }
 
 ?>
+
+<ul class="pagination">
+    <?php if ($paginator->getPrevUrl()): ?>
+        <li><a href="<?php echo $paginator->getPrevUrl(); ?>">&laquo; Previous</a></li>
+    <?php endif; ?>
+
+    <?php foreach ($paginator->getPages() as $page): ?>
+        <?php if ($page['url']): ?>
+            <li <?php echo $page['isCurrent'] ? 'class="active"' : ''; ?>>
+                <a href="<?php echo $page['url']; ?>"><?php echo $page['num']; ?></a>
+            </li>
+        <?php else: ?>
+            <li class="disabled"><span><?php echo $page['num']; ?></span></li>
+        <?php endif; ?>
+    <?php endforeach; ?>
+
+    <?php if ($paginator->getNextUrl()): ?>
+        <li><a href="<?php echo $paginator->getNextUrl(); ?>">Next &raquo;</a></li>
+    <?php endif; ?>
+</ul>
+
+<p>
+    <?php echo $paginator->getTotalItems(); ?> found.
+
+    Showing
+    <?php echo $paginator->getCurrentPageFirstItem(); ?>
+    -
+    <?php echo $paginator->getCurrentPageLastItem(); ?>.
+</p>
 </body>
 </html>
